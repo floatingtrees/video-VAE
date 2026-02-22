fix bug in frame weighting

claude_distributed/distributed_train.py
@@ -257,8 +257,10 @@ if __name__ == "__main__":
         target_kept = jax.lax.stop_gradient(
             reduce(rl_mask_f, "b p time -> b p 1", "sum")) / 2 # 16 for length 32, etc
         frame_weight = jnp.where(actions > 0.5, target_kept / num_kept, target_kept / num_dropped)
-        frame_weight = jnp.where(rl_mask, frame_weight, 1.0)
 
+
+        raw_probs_masked = jnp.where(rl_mask, raw_probs, 1.0)
+        raw_trajectory_probs = reduce(raw_probs_masked, "b p time -> b p 1", "prod")
 
         disadvantages = rearrange(disadvantages, "b p -> b p 1")
 