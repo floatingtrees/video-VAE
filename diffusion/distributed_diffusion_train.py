2c550fcbc65fc2505c8d9fe302fa667d743f5a39
floatingtrees <juriyanghai@gmail.com>
2026-03-11 16:34:26 -0700

    fixed bigs

--- diffusion/distributed_diffusion_train.py
+++ diffusion/distributed_diffusion_train.py
@@ -299,15 +299,8 @@ if __name__ == "__main__":
     master_weights = nnx.merge(gdef, state)
     if args.model_path is not None:
         master_path = f"{args.model_path}_master"
-        if process_index == 0:
-            from etils import epath
-            master_exists = epath.Path(master_path).exists()
-        else:
-            master_exists = False
-        master_exists = bool(jax.experimental.multihost_utils.broadcast_one_to_all(
-            np.array(master_exists)
-        ))
-        if master_exists:
+        from etils import epath
+        if epath.Path(master_path).exists():
             load_model(master_weights, master_path)
         else:
             if process_index == 0:
